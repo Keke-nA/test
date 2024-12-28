f1fbc3e05af17fbc43463475128c29f13c5c1f89
gxy <gxyluvyxg@qq.com>
2024-12-28 13:39:58 +0800

    yigeceshi

--- test.cpp
+++ test.cpp
@@ -7,6 +7,8 @@ int main()
     std::cin >> gao;
     std::cout << gao << std::endl;
     std::cout << "hello world" << '\n';
-    std;
+    std::cout << "hello" << '\n';
+    std::cout << "its ok!" <<'\n';
+    std::cout << "hello";
     return 0;
 }
